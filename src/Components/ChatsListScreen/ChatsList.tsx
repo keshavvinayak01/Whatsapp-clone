97717a059fa87be7ed6e0e482af04cd63cd91008
Keshav Vinayak <keshavvinayakjha@gmail.com>
2019-10-05 02:10:23 +0530

    Added moment styling

--- src/Components/ChatsListScreen/ChatsList.tsx
+++ src/Components/ChatsListScreen/ChatsList.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { chats } from '../../db';
+import moment from 'moment';
 
 const ChatsList : React.FC = () => (
     <div>
@@ -12,7 +13,7 @@ const ChatsList : React.FC = () => (
                         {chat.lastMessage && (
                         <>
                             <div>{chat.lastMessage.content}</div>
-                            <div>{chat.lastMessage.createdAt}</div>
+                            <div>{moment(chat.lastMessage.createdAt)}</div>
                         </>
                         )}
                     </li>
