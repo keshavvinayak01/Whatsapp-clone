cf930f2945cc220416394d5743353b71517fac34
Keshav Vinayak <keshavvinayakjha@gmail.com>
2019-10-05 01:53:08 +0530

    Basic inital app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<div>WhatsApp Clone</div>
+			<div>
+				<ul>
+					<li>
+						<img src="https://randomuser.me/api/portraits/thumb/men/1.jpg" alt="Profile" />
+						<div>Keshav Vinayak</div>
+						<div>You on your way?</div>
+						<div>10:25</div>
+					</li>
+					<li>
+						<img src="https://randomuser.me/api/portraits/thumn/men/2.jpg" alt = "Profile" />
+						<div>Byran Walker</div>
+						<div>Hey, it's me</div>
+						<div>13:27</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
 }
 
 export default App;
