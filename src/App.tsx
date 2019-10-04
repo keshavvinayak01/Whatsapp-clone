3ae1b1ffc9e6a29e211e8d778de87657eca0d66d
Keshav Vinayak <keshavvinayakjha@gmail.com>
2019-10-05 02:21:44 +0530

    Reorganization[2]

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ChatsListScreen from './Components/ChatsListScreen';
+
+const App: React.FC = () => {
+	return (
+		<div>
+			<ChatsListScreen />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/Components/ChatsListScreen/ChatsNavbar.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const ChatsNavbar : React.FC = () => <div>WhatsApp Clone</div>
+
+export default ChatsNavbar;
