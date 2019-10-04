d86239e34cc389d32ac2783bff1e6711fd296924
Keshav Vinayak <keshavvinayakjha@gmail.com>
2019-10-05 01:56:14 +0530

    Created ChatsList

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
 import React from 'react';
+import ChatsList from './Components/ChatsList';
 
 const App: React.FC = () => {
 	return (
 		<div>
 			<div>WhatsApp Clone</div>
-			<div>
-				<ul>
-					<li>
-						<img src="https://randomuser.me/api/portraits/thumb/men/1.jpg" alt="Profile" />
-						<div>Keshav Vinayak</div>
-						<div>You on your way?</div>
-						<div>10:25</div>
-					</li>
-					<li>
-						<img src="https://randomuser.me/api/portraits/thumn/men/2.jpg" alt = "Profile" />
-						<div>Byran Walker</div>
-						<div>Hey, it's me</div>
-						<div>13:27</div>
-					</li>
-				</ul>
-			</div>
+			<ChatsList />
 		</div>
 	);
 }
