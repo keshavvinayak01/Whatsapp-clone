e00c2fc039239ea852d6425fed2522f3062a38ed
Keshav Vinayak <keshavvinayakjha@gmail.com>
2019-10-05 10:57:12 +0530

    Styling added to ChatsList component

--- frontend/src/Components/ChatsListScreen/ChatsList.tsx
+++ frontend/src/Components/ChatsListScreen/ChatsList.tsx
@@ -2,26 +2,89 @@ import React from 'react'
 import { chats } from '../../db';
 import moment from 'moment';
 import { List, ListItem } from '@material-ui/core';
+import styled from 'styled-components';
 
-const ChatsList : React.FC = () => (
-    <div>
-        <List>
+const Container = styled.div`
+height : calc(100% - 50px);
+overflow-y : overlay;
+`;
+
+const StyledList = styled(List)`
+padding : 0 !important;
+`;
+
+const ChatName = styled.div`
+margin-top : 5px;
+`
+
+const StyledListItem = styled(ListItem)`
+height: 76px;
+padding : 0 15px;
+display : flex;
+`;
+
+const ChatPicture = styled.img`
+height : 50px;
+width : 50px;
+object-fit : cover;
+border-radius : 50%;
+`;
+
+const ChatInfo = styled.div`
+width : calc(100% - 50px);
+height : 46px;
+padding : 15px 0;
+margin-left : 10px;
+border-bottom : 0.5px solid silver;
+position : relative;
+`;
+
+const MessageContent = styled.div`
+color : gray;
+font-size : 15px;
+matgin-top : 5px;
+text-overflow : ellipsis;
+overflow : hidden;
+white-space : nowrap;
+`;
+
+const MessageDate = styled.div`
+position : absolute;
+color : gray;
+top : 20px;
+right : 0;
+font-size : 13px;
+`;
+
+
+const ChatsList = () => (
+    <Container>
+        <StyledList>
             {
                 chats.map(chat => (
-                    <ListItem key={chat.id} button>
-                        <img src="{chat.picture}" alt="Profile"/>
-                        <div>{chat.name}</div>
-                        {chat.lastMessage && (
-                        <>
-                            <div>{chat.lastMessage.content}</div>
-                            <div>{moment(chat.lastMessage.createdAt)}</div>
-                        </>
-                        )}
-                    </ListItem>
+                    <StyledListItem key={chat.id} button>
+                        <ChatPicture src={chat.picture} alt="Profile">
+                            <ChatInfo>
+                                <ChatName>{chat.name}</ChatName>
+                                {
+                                    chat.lastMessage && (
+                                        <>
+                                            <MessageContent>
+                                                {chat.lastMessage.content}
+                                            </MessageContent>
+                                            <MessageDate>
+                                                {moment(chat.lastMessage.createdAt).format('HH:mm')}
+                                            </MessageDate>
+                                        </>
+                                    )
+                                }
+                            </ChatInfo>
+                        </ChatPicture>
+                    </StyledListItem>
                 ))
             }
-        </List>>
-    </div>
-);
+        </StyledList>
+    </Container>
+)
 
 export default ChatsList;
