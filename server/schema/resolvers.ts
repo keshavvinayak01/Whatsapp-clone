3e7a81770b37a68bf2aa99f987f7b34c48979d81
Keshav Vinayak <keshavvinayakjha@gmail.com>
2019-10-06 01:43:56 +0530

    Created query resolver

---
+++ server/schema/resolvers.ts
@@ -0,0 +1,21 @@
+import { DateTimeResolver, URLResolver } from 'graphql-scalars';
+import { chats, messages } from '../db';
+
+const resolvers = {
+    Date : DateTimeResolver,
+    URL :URLResolver,
+
+    Chat : {
+        lastMessage(chat : any){
+            return messages.find(m => m.id === chat.lastMessage)
+        },
+    },
+    
+    Query : {
+        chats(){
+            return chats;
+        },
+    },
+};
+
+export default resolvers;
