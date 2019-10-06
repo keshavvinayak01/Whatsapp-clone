764249a2bf16d130571db37285b371ab0746b962
Keshav Vinayak <keshavvinayakjha@gmail.com>
2019-10-06 23:43:22 +0530

    Added routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import ChatRoomScreen from './components/ChatRoomScreen';
 import ChatsListScreen from './Components/ChatsListScreen';
 
 const App: React.FC = () => {
 	return (
-		<div>
-			<ChatsListScreen />
-		</div>
+		<BrowserRouter>
+			<Switch>
+				<Route exact path="/chats" component={ChatsListScreen} />
+				<Route exact path="/chats/:chatId" component={ChatRoomScreen} />
+			</Switch>
+			<Route exact path="/" render={redirectToChats} />
+		</BrowserRouter>
 	);
 }
+
+const redirectToChats = () => <Redirect to="chats" />
 
 export default App;
