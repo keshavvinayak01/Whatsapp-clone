9b3fa3ba3db4389c5f0493563baf53d93483278a
Keshav Vinayak <keshavvinayakjha@gmail.com>
2019-10-08 00:45:43 +0530

    History prop passed

--- frontend/src/Components/ChatsListScreen/index.tsx
+++ frontend/src/Components/ChatsListScreen/index.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import ChatsNavbar from './ChatsNavbar';
 import ChatsList from './ChatsList';
 import styled from 'styled-components';
+import { History } from 'history';
+
+interface ChatsListScreenProps {
+    history : History;
+}
 
 const Container = styled.div`
     height : 100vh;
 `;
-const ChatsListScreen : React.FC = () => (
+const ChatsListScreen : React.FC<ChatsListScreenProps> = ({ history }) => (
     <Container>
         <ChatsNavbar />
-        <ChatsList />
+        <ChatsList history={history} />
     </Container>
 );
 
