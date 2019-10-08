e53142847c9da211289082c9dd7802a8434b0252
Keshav Vinayak <keshavvinayakjha@gmail.com>
2019-10-08 11:44:56 +0530

    Wrapped with ApolloProvider

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,10 @@
 import { MuiThemeProvider,createMuiTheme } from '@material-ui/core/styles';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider }from '@apollo/react-hooks';
 import './index.css';
 import App from './App';
+import client from './client';
 import * as serviceWorker from './serviceWorker';
 
 
@@ -14,7 +16,9 @@ const theme = createMuiTheme({
 });
 ReactDOM.render(
     <MuiThemeProvider theme = {theme}>
+    <ApolloProvider client = {client} >
         <App />
+    </ApolloProvider>
     </MuiThemeProvider>,
     document.getElementById('root')
 );
