e8829e6b6449db976baadf0f8de9cc8341ec6c13
Keshav Vinayak <keshavvinayakjha@gmail.com>
2019-10-08 19:38:52 +0530

    Rewrite for fragments[1]

--- frontend/src/graphql/fragments/index.ts
+++ frontend/src/graphql/fragments/index.ts
@@ -1,3 +1,3 @@
-export {default as Chat} from './chat.fragment';
+export {default as chat} from './chat.fragment';
 export {default as fullChat} from './fullChat.fragment';
 export {default as message} from './message.fragment';
