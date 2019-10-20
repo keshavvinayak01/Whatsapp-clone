789c9b9d08ce6625a1166809a7148e899f605b33
Keshav Vinayak <keshavvinayakjha@gmail.com>
2019-10-20 23:17:55 +0530

    Add withAuth() route wrapper

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,25 +9,22 @@ import AuthScreen from './components/AuthScreen';
 import ChatRoomScreen from './components/ChatRoomScreen';
 import ChatsListScreen from './components/ChatsListScreen';
 import AnimatedSwitch from './components/AnimatedSwitch';
-import { useCacheService } from './services/cache.service';
+import { withAuth } from './services/auth.service';
 
 const App: React.FC = () => {
-  useCacheService();
-
   return (
     <BrowserRouter>
       <AnimatedSwitch>
         <Route exact path="/sign-(in|up)" component={AuthScreen} />
         <Route exact path="/chats" component={ChatsListScreen} />
+
         <Route
-          exact
-          path="/chats/:chatId"
-          component={({
-            match,
-            history,
-          }: RouteComponentProps<{ chatId: string }>) => (
+        exact path="/chats/:chatId"
+        component = {withAuth(
+          ({ match, history } : RouteComponentProps<{ chatId : string }>) => (
             <ChatRoomScreen chatId={match.params.chatId} history={history} />
-          )}
+          )
+        )}
         />
       </AnimatedSwitch>
       <Route exact path="/" render={redirectToChats} />
