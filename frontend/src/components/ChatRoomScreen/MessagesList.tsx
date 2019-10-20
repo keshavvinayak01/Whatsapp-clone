ecfdd306d66fd1acafbdfa10d3e30f5b8ca2bd20
Keshav Vinayak <keshavvinayakjha@gmail.com>
2019-10-20 18:22:05 +0530

    Distinguish Message[1]

--- frontend/src/components/ChatRoomScreen/MessagesList.tsx
+++ frontend/src/components/ChatRoomScreen/MessagesList.tsx
@@ -2,7 +2,7 @@ import moment from 'moment';
 import React from 'react';
 import { useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const Container = styled.div`
   display: block;
@@ -11,9 +11,11 @@ const Container = styled.div`
   padding: 0 15px;
 `;
 
+type StyledProp = {
+  isMine : any;
+};
+
 const MessageItem = styled.div`
-  float: right;
-  background-color: #dcf8c6;
   display: inline-block;
   position: relative;
   max-width: 100%;
@@ -30,16 +32,36 @@ const MessageItem = styled.div`
   }
 
   &::before {
-    background-image: url(/assets/message-mine.png);
     content: '';
     position: absolute;
     bottom: 3px;
     width: 12px;
     height: 19px;
-    right: -11px;
     background-position: 50% 50%;
     background-repeat: no-repeat;
     background-size: contain;
+  }
+
+  ${(props : StyledProp) => 
+    props.isMine
+      ? css`
+        float : right;
+        background-color : #dcf8c6;
+
+        &::before {
+          right : -11px;
+          background-image : url(/assets/message-mine.png);
+        }
+      `
+      : css`
+        float : left;
+        background-color : #fff;
+
+        &::before {
+          left: -11px;
+          background-image : url(/assets/message-other.png) 
+        }
+      `
   }
 `;
 
@@ -83,7 +105,10 @@ const MessagesList: React.FC<MessagesListProps> = ({ messages }) => {
   return (
     <Container ref={selfRef}>
       {messages.map((message: any) => (
-        <MessageItem data-testid="message-item" key={message.id}>
+        <MessageItem 
+          data-testid="message-item" 
+          key={message.id}
+          iMine={message.isMine}>
           <Contents data-testid="message-content">{message.content}</Contents>
           <Timestamp data-testid="message-date">
             {moment(message.createdAt).format('HH:mm')}
