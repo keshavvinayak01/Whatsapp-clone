4c33f2fbd2be6ab3a2c3482ada55c8c42a839228
Keshav Vinayak <keshavvinayakjha@gmail.com>
2019-10-20 12:20:39 +0530

    Chat resolver for current User

--- server/schema/resolvers.ts
+++ server/schema/resolvers.ts
@@ -16,16 +16,26 @@ const resolvers: Resolvers = {
     recipient(message){
       return users.find(u => u.id === message.recipient) || null;
     }
+    isMine(message, args,{ currentUser }){
+      return message.sender === currentUser.id;
+    }
   },
 
   Chat: {
-    name(){
-      // Resolve in relation to current user  
-      return null;
+    name(chat, args,{ currentUser }){
+      if(!currentUser) return null;
+      const participantId = chat.participants.find(p => p !== currentUser.id);
+      
+      if(!participantId) return null;
+      const participant = users.find(u => u.id === participantId);
+      return participant ? participant.name : null;
     }
-    picture(){
-      // Resolve in relation to current user
-      return null;
+    picture(chat, args, { currentUser }){
+      if(!currentUser) return null;
+      const participantId = chat.participants.find(p => p !== currentUser.id);
+      if(!participantId) return null;
+      const participant = users.find(u => u.id === participantId);
+      return participant? participant.picture : null;
     }
     messages(chat) {
       return messages.filter(m => chat.messages.includes(m.id));
@@ -44,30 +54,35 @@ const resolvers: Resolvers = {
   },
 
   Query: {
-    chats() {
-      return chats;
+    chats(root, args, { currentUser }) {
+      if(!currentUser) return [];
+      return chats.filter(c => c.participants.includes(currentUser.id));
     },
 
-    chat(root, { chatId }) {
-      return chats.find(c => c.id === chatId) || null;
+    chat(root, { chatId }, { currentUser }) {
+      if(!currentUser) return null;
+      const chat = chats.find(c => c.id === chatId);
+      if(!chat) return null;
+      return chat.participants.includes(currentUser.id) ? chat : null;
     },
   },
 
   Mutation: {
-    addMessage(root, { chatId, content }, { pubsub }) {
+    addMessage(root, { chatId, content }, { currentUser, pubsub }) {
+      if(!currentUser) return null;
       const chatIndex = chats.findIndex(c => c.id === chatId);
 
       if (chatIndex === -1) return null;
 
       const chat = chats[chatIndex];
-
+      if(!chat.participants.includes(currentUser.id)) return null;
       const messagesIds = messages.map(currentMessage => Number(currentMessage.id));
       const messageId = String(Math.max(...messagesIds) + 1);
       const message: Message = {
         id: messageId,
         createdAt: new Date(),
-        sender : '', // Fill in later
-        recipient : '', // Fill in later
+        sender : currentUser.id, // Fill in later
+        recipient : chat.participants.find(p => p !== currentUser.id) as string, // Fill in later
         content,
       };
 
