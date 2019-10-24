40e1fa9881258ffa770ed51cf6658e7522d27a09
Keshav Vinayak Jha <keshavvinayakjha@gmail.com>
2019-10-24 13:02:43 +0530

    Added delete button to ChatNav

--- frontend/src/components/ChatsListScreen/ChatsNavbar.tsx
+++ frontend/src/components/ChatsListScreen/ChatsNavbar.tsx
@@ -1,48 +1,102 @@
+import Button from '@material-ui/core/Button';
+import Toolbar from '@material-ui/core/Toolbar';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import DeleteIcon from '@material-ui/icons/Delete';
+import gql from 'graphql-tag';
 import React from 'react';
-import { Toolbar } from '@material-ui/core';
-import { Button, Toolbar } from '@material-ui/core';
-import styled from 'styled-components';
-import SignOutIcon from '@material-ui/icons/PowerSettingsNew';
 import { useCallback } from 'react';
-import { useSignOut } from '../../services/auth.service';
+import styled from 'styled-components';
 import { History } from 'history';
+import { useRemoveChatMutation } from '../../graphql/types';
 
 const Container = styled(Toolbar)`
-  display : flex;
+  padding: 0;
+  display: flex;
+  flex-direction: row;
   background-color: var(--primary-bg);
   color: var(--primary-text);
-  font-size: 20px;
-  line-height: 40px;
 `;
 
-const Title = styled.div`
-  flex : 1;
+const BackButton = styled(Button)`
+  svg {
+    color: var(--primary-text);
+  }
 `;
 
-const LogoutButton = styled(Button)`
-  color : var(--primary-text) !important;
+const Rest = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
 `;
 
-interface ChildComponentProps {
-  history : History;
-};
+const Picture = styled.img`
+  height: 40px;
+  width: 40px;
+  margin-top: 3px;
+  margin-left: -22px;
+  object-fit: cover;
+  padding: 5px;
+  border-radius: 50%;
+`;
 
-const ChatsNavbar: React.FC<ChildComponentProps> = ({ history }) => {
-  const signOut = useSignOut();
-  const handleSignOut = useCallback(() => {
-    signOut().then(() => {
-      history.replace('/sign-in');
-    });
-  }, [history, signOut]);
-  
-  return (
-    <Container>
-      <Title>WhatsApp Clone</Title>
-      <LogoutButton data-testid="sign-out-button" onClick={handleSignOut}>
-        <SignOutIcon />
-      </LogoutButton>
-    </Container>
-  );
+const Name = styled.div`
+  line-height: 56px;
+`;
+
+const DeleteButton = styled(Button)`
+  color: var(--primary-text) !important;
+`;
+
+export const removeChatMutation = gql`
+  mutation RemoveChat($chatId: ID!) {
+    removeChat(chatId: $chatId)
+  }
+`;
+
+interface ChatNavbarProps {
+  history: History;
+  chat: {
+    picture?: string | null;
+    name?: string | null;
+    id: string;
+  };
 }
 
-export default ChatsNavbar;
+const ChatNavbar: React.FC<ChatNavbarProps> = ({ chat, history }) => {
+  const [removeChat] = useRemoveChatMutation({
+    variables: {
+      chatId: chat.id,
+    },
+  });
+
+  const handleRemoveChat = useCallback(() => {
+    removeChat().then(() => {
+      history.replace('/chats');
+    });
+  }, [removeChat, history]);
+
+  const navBack = useCallback(() => {
+    history.replace('/chats');
+  }, [history]);
+
+  return (
+    <Container>
+      <BackButton data-testid="back-button" onClick={navBack}>
+        <ArrowBackIcon />
+      </BackButton>
+      {chat && chat.picture && chat.name && (
+        <React.Fragment>
+          <Picture data-testid="chat-picture" src={chat.picture} />
+          <Name data-testid="chat-name">{chat.name}</Name>
+        </React.Fragment>
+      )}
+      <Rest>
+        <DeleteButton data-testid="delete-button" onClick={handleRemoveChat}>
+          <DeleteIcon />
+        </DeleteButton>
+      </Rest>
+    </Container>
+  );
+};
+
+export default ChatNavbar;
