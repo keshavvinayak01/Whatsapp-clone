ee7cbfc39dbf1d9fe3ab72f629263f8476204533
Keshav Vinayak Jha <keshavvinayakjha@gmail.com>
2019-10-25 16:15:24 +0530

    JSON expiration env

--- server/env.ts
+++ server/env.ts
@@ -1,2 +1,5 @@
+export const expiration = process.env.JWT_EXPIRATION_MS 
+    ? parseInt(process.env.JWT_EXPIRATION_MS)
+    : 24*60*60*1000
 export const origin = process.env.ORIGIN || 'http://localhost:3000';
 export const port = process.env.ORIGIN || 4000;
