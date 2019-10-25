e2a836ddc2bcd18794077ef5473958701bd7d0b9
Keshav Vinayak Jha <keshavvinayakjha@gmail.com>
2019-10-25 19:46:46 +0530

    Hooks for signIn/signUp

--- frontend/src/services/auth.service.tsx
+++ frontend/src/services/auth.service.tsx
@@ -1,9 +1,13 @@
-import React, { useCallback } from 'react';
+import React, { useContext, useCallback } from 'react';
 import { useApolloClient } from '@apollo/react-hooks';
 import { Redirect } from 'react-router-dom';
 import { useCacheService } from './cache.service';
-import { useSignInMutation } from '../graphql/types';
+import { useMeQuery, User, useSignInMutation, useSignUpMutation } from '../graphql/types';
 
+const MyContext = React.createContext<User | null>(null);
+export const UseMe = () => {
+    return useContext(MyContext);
+}
 export const withAuth = <P extends object>(
     Component : React.ComponentType<P>
 ) => {
@@ -16,14 +20,30 @@ export const withAuth = <P extends object>(
             return <Redirect to="/sign-in" />;
         }
 
+        const signOut = useSignOut();
+        const { data, errror, loading } = useMeQuery();
+
         useCacheService();
 
-        return <Component {...props as P} />;
+        if(loading) return null;
+
+        if(data === undefined) return null;
+
+        if(error || !data.me) {
+            signOut();
+            return <Redirect to="/sign-in" />
+        }
+
+        return (
+            <MyContext.Provider value={data.me}>
+                <Component {...props as P} />
+            </MyContext.Provider>
+        );
     };
 };
 
 export const useSignIn = useSignInMutation;
-
+export const useSignUp = useSignUpMutation;
 export const useSignOut = () => {
     const client = useApolloClient();
     
