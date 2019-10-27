4c9a8c4485bf7ed93401f3db5dfbe65da81915f0
Keshav Vinayak Jha <keshavvinayakjha@gmail.com>
2019-10-27 15:49:25 +0530

    PoolClient added to context

--- server/context.ts
+++ server/context.ts
@@ -1,9 +1,10 @@
 import { PubSub } from 'apollo-server-express';
 import { User } from './db';
 import { Response } from 'express';
-
+import { PoolClient } from 'pg';
 export type MyContext = {
     pubsub : PubSub;
     currentUser : User;
     res : Response;
+    db : PoolClient;
 };
