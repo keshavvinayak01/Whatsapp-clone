9e009c19b6839e156e7e8c16875fa361a94573ed
Keshav Vinayak Jha <keshavvinayakjha@gmail.com>
2019-10-27 16:43:49 +0530

    Removed in-memory test[7]

--- server/tests/queries/getUser.test.ts
+++ server/tests/queries/getUser.test.ts
@@ -1,15 +1,27 @@
 import { createTestClient } from 'apollo-server-testing';
 import { ApolloServer, gql } from 'apollo-server-express';
 import schema from '../../schema';
-import { users } from '../../db';
+import { users, pool } from '../../db';
+import { MyContext } from '../../context';
+import sql from 'sql-template-strings';
 
 describe('Query.getUsers', () => {
     it('should fetch all users except the one signed-in', async () => {
-        let currentUser = users[0];
-
+        const firstUserQuery = await pool.query(sql`
+            select * from users where id  = 1
+        `);
+        let currentUser = firstUserQuery.rows[0];
+        const db = await pool.connect();
         const server = new ApolloServer({
             schema,
-            context : () => ({ currentUser }),
+            context : async () => ({
+                currentUser,
+                db : await pool.connect() 
+            }),
+            formatResponse : (res : any, { context } : { context : MyContext}) => {
+                context.db.release();
+                return res;
+            }
         });
 
         const { query } = createTestClient(server);
@@ -30,7 +42,10 @@ describe('Query.getUsers', () => {
         expect(res.errors).toBeUndefined();
         expect(res.data).toMatchSnapshot();
 
-        currentUser = users[1];
+        const secondUserQuery = await pool.query (
+            sql`select * from users where id = 2`
+        );
+        currentUser = secondUserQuery.rows[0];
 
         res = await query({
             query : gql`
