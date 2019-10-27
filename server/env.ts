405b8a24834c4bba151902b399b41f44c72983e2
Keshav Vinayak Jha <keshavvinayakjha@gmail.com>
2019-10-27 16:16:10 +0530

    Created secret, optional reset

--- server/env.ts
+++ server/env.ts
@@ -3,3 +3,5 @@ export const expiration = process.env.JWT_EXPIRATION_MS
     : 24*60*60*1000
 export const origin = process.env.ORIGIN || 'http://localhost:3000';
 export const port = process.env.ORIGIN || 4000;
+export const secret = process.env.JWT_SECRET || '70p53cr37';
+export const resetDb = process.env.RESET_DB || false;
